7b3d546a202ce86ce42c9a3313a61004fca33bcb
opilane <opilane@HP-CZC321706P>
2026-04-01 10:34:15 +0300

    context and instalations

---
+++ E-pood/E-pood/Data/ShopContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_pood.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options) { }
+    }
+}
