eb0d1fb64fa25bc7bc11011d118ad572a8405a0b
opilane <opilane@HP-CZC321706P>
2026-04-01 11:33:33 +0300

    project files, context

---
+++ E-pood.ApplicationServices/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_pood.ApplicationServices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ E-pood.Data/ShopContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_pood.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options) { }
+    }
+}
