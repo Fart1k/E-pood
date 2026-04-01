b89d7dcf9c791340ecf8fbe63ad4ced90091f9f4
opilane <opilane@HP-CZC321706P>
2026-04-01 10:53:41 +0300

    context deleted

--- E-pood/E-pood/Data/ShopContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace E_pood.Data
-{
-    public class ShopContext : DbContext
-    {
-        public ShopContext(DbContextOptions<ShopContext> options) : base(options) { }
-    }
-}
