0ace220e0164a586997ba6d3b7e47f949232e9ee
opilane <opilane@HP-CZC3217070>
2026-04-14 08:54:43 +0300

    project files, context

---
+++ Epood/Data/ShopContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Epood.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options) { }
+    }
+}
