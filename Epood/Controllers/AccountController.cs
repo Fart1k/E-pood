759d0e785fc8e6cef84a61b747ed502f05e02816
User <User@DESKTOP-N36668M>
2026-04-27 13:25:30 +0300

    Created method in account controller for settings

--- Epood/Controllers/AccountController.cs
+++ Epood/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
 ﻿using Epood.Models;
+using Epood.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 
 namespace Epood.Controllers
 {
@@ -29,5 +31,45 @@ namespace Epood.Controllers
             return View(user);
             
         }
+
+        // Settings
+        [HttpGet]
+        public async Task<IActionResult> Settings()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var vm = new AccountSettingsViewModel
+            {
+                Email = user.Email,
+                UserName = user.UserName,
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Settings(AccountSettingsViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+
+            user.Email = vm.Email;
+            user.UserName = vm.UserName;
+
+            await _userManager.UpdateAsync(user);
+
+            if (!string.IsNullOrEmpty(vm.NewPassword))
+            {
+                var passwordToken = await _userManager.GeneratePasswordResetTokenAsync(user);
+                await _userManager.ResetPasswordAsync(user, passwordToken, vm.NewPassword);
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
