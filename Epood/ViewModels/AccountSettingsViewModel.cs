89c397df2575b4d4c34cdf22e3e7c4b3c5626582
User <User@DESKTOP-N36668M>
2026-04-27 13:25:11 +0300

    ViewModel for Settings

---
+++ Epood/ViewModels/AccountSettingsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Epood.ViewModels
+{
+    public class AccountSettingsViewModel
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string? NewPassword{ get; set; }
+    }
+}
