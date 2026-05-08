6326bae094490e7b100b6d7757aa7a88132489ad
User <User@DESKTOP-N36668M>
2026-05-08 13:45:45 +0300

    Can now remove from cart, change quantity from cart and go to product page from cart

--- Epood/Controllers/CartController.cs
+++ Epood/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
 
 namespace Epood.Controllers
 {
@@ -18,7 +19,7 @@ namespace Epood.Controllers
             _shopContext = shopContext;
             _userManager = userManager;
         }
-        
+
         public async Task<IActionResult> Index()
         {
             var user = await _userManager.GetUserAsync(User);
@@ -30,7 +31,8 @@ namespace Epood.Controllers
 
             return View(cartItems);
         }
-        
+
+        //AddToCart
         [HttpPost]
         public async Task<IActionResult> AddToCart(int productId)
         {
@@ -59,6 +61,51 @@ namespace Epood.Controllers
 
             return RedirectToAction("Index");
 
+        }
+
+        //UpdateQuantity
+        [HttpPost]
+        public async Task<IActionResult> UpdateQuantity(int cartItemId, int quantity)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var cartItem = _shopContext.CartItems
+                .FirstOrDefault(x => x.Id == cartItemId && x.UserId == user.Id);
+
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            if (quantity < 1)
+            {
+                quantity = 1;
+            }
+
+            cartItem.Quantity = quantity;
+
+            await _shopContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        //RemoveFromCart
+        [HttpPost]
+        public async Task<IActionResult> Remove(int cartItemId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var cartItem = _shopContext.CartItems
+                .FirstOrDefault(x => x.Id == cartItemId && x.UserId == user.Id);
+
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            _shopContext.CartItems.Remove(cartItem);
+            await _shopContext.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
