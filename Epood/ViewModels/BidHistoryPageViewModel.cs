9f95282983062ff0e581bf5d6f0399a95ef192c7
User <User@DESKTOP-N36668M>
2026-06-02 13:00:20 +0300

    ViewModels for BidHistory

---
+++ Epood/ViewModels/BidHistoryPageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Epood.ViewModels
+{
+    public class BidHistoryPageViewModel
+    {
+        public string ProductName { get; set; }
+        public List<BidHistoryViewModel> Bids { get; set; }
+    }
+}
